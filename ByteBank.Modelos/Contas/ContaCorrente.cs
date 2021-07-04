d20b58abbe980bdc6139d9e0397ca36615f61c30
Douglas Diehl Lutz <douglas.lutz@codeminer42.com>
2021-07-04 11:58:36 -0300

    5.3 - Adicionando documentação na classe ContaCorrente

--- ByteBank.Modelos/Contas/ContaCorrente.cs
+++ ByteBank.Modelos/Contas/ContaCorrente.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Modelos.Contas
 {
+    /// <summary>
+    /// Define uma Conta Corrente do banco ByteBank.
+    /// </summary>
     public class ContaCorrente
     {
         private double _saldo;
@@ -22,7 +25,12 @@ namespace ByteBank.Modelos.Contas
 
         public static int TotalDeContasCriadas { get; private set; }
 
-
+        /// <summary>
+        /// Cria uma instância de Conta Corrente com os argumentos recebidos.
+        /// </summary>
+        /// <param name="titular"> Representa a referência da propriedade <see cref="Titular"/>. </param>
+        /// <param name="agencia"> Representa o valor da propriedade <see cref="Agencia"/>. </param>
+        /// <param name="numero"> Representa o valor da propriedade <see cref="Numero"/>. </param>
         public ContaCorrente(Cliente titular, string agencia, string numero)
         {
             Titular = titular;
@@ -34,7 +42,12 @@ namespace ByteBank.Modelos.Contas
             Console.WriteLine("Parabéns pela nova conta " + Titular.Nome + "! O saldo é de " + Saldo + ".");
         }
 
-
+        /// <summary>
+        /// Realiza o saque e atualiza o valor da propriedade <see cref="Saldo"/>.
+        /// </summary>
+        /// <exception cref="ArgumentException"> Exceção lançada quando um valor negativo é atribuido ao <paramref name="valor"/>. </exception>
+        /// <exception cref="SaldoInsuficienteException"> Exceção lançada quando o valor de <paramref name="valor"/> é maior que o <see cref="Saldo"/>. </exception>
+        /// <param name="valor"> Representa o valor do saque, deve ser maior que 0 e menor ou igual ao <see cref="Saldo"/>. </param>
         public void Sacar(double valor)
         {
             Console.WriteLine(Titular.Nome + " está sacando R$" + valor + " de sua conta.");
